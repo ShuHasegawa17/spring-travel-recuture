b313cc63cca10e3355d772cba7c724ba2e740812
shuhasegawa <shuhasegawa0430@gmail.com>
2023-09-10 22:55:38 +0900

    traverアプリ

---
+++ src/main/java/com/example/travel/repository/HouseRepository.java
@@ -0,0 +1,12 @@
+package com.example.travel.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.travel.entity.House;
+
+public interface HouseRepository extends JpaRepository<House, Integer>{
+	// 部分一致のメソッドは独自で定義（like演算子）
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
+}
